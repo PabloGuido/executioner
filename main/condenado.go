81e04e33129ea33cc8c118ec564935e6d9c947e3
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-09-22 16:05:05 -0300

    Executioner al repo.-

---
+++ main/condenado.go
@@ -0,0 +1,61 @@
+components {
+  id: "condenado"
+  component: "/main/condenado.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "hacha_2"
+  type: "sound"
+  data: "sound: \"/main/hacha_2.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "hacha"
+  type: "sound"
+  data: "sound: \"/main/hacha.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/datos.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.game_over = false
+M.cuenta = 0
+M.cabeza_en_posicion = true
+
+return M
